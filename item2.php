c72bd8f1cbf3343c845b5d18175fae4f2c851728
gabriellamarins <81614041+gabriellamarins@users.noreply.github.com>
2021-05-20 11:09:54 +0200

    Header + footer
    add header + footer

---
+++ item2.php
@@ -0,0 +1,15 @@
+<?php
+include "header.php"?>
+
+<?php
+$name = "Gabriella";
+$prix = 15;
+$img = 'https://wallpaperaccess.com/full/428160.jpg';
+
+
+echo "$name </br> $prix  <img src = '$img' alt = cat/>";
+
+?>
+
+<?php
+include "footer.php"?>
