2c78f6a3d32cdcb708776550486332134de1e00f
gabriellamarins <81614041+gabriellamarins@users.noreply.github.com>
2021-05-20 16:57:01 +0200

    last commit of the day

--- key-catalog.php
+++ key-catalog.php
@@ -12,7 +12,7 @@ $puma = [
     "price" => 200,
     "weight" => 400,
     "discount" => 10,
-    "picture" => puma.jpg,
+    "picture" => 'https://images-na.ssl-images-amazon.com/images/I/819tIj8wVmL._AC_UX395_.jpg',
 ];
 
 $veja = [
@@ -20,40 +20,46 @@ $veja = [
     "price" => 200,
     "weight" => 400,
     "discount" => 10,
-    "picture" => veja.jpg,
+    "picture" => 'https://m.media-amazon.com/images/I/61v5ACBpE-L._AC_SX395._SX._UX._SY._UY_.jpg',
 ];
 
 ?>
 
-<htm>
+<html lang="english">
     <head>
         <title>
             Catalogue
         </title>
     </head>
     <body>
-    <h1>
+    <h1 style>
         Sneakears
     </h1>
 
 
-    <h2> Nike <?php echo $nike ['name'] ?> </h2>
-
-           <br>
-            <br>
-            <img src='<?php echo $nike ['picture']?>' width = "400">
-    <br>
-            Prix: <?php echo $nike ['price']?> euro
-    <br>
-            Reduction: <?php echo $nike ['discount'] ?>
-
-
-    
-
-
-
-
+    <table style="width:100%">
+        <tr>
+            <th>Nike <?php echo $nike ['name'] ?> </th>
+            <th>Puma <?php echo $puma ['name'] ?> </th>
+            <th>Veja <?php echo $veja ['name'] ?> </th>
+        </tr>
+<tr>
+    <td> <img src='<?php echo $nike ['picture']?>' width = "400"></td>
+    <td> <img src='<?php echo $puma ['picture']?>' width = "400"></td>
+    <td> <img src='<?php echo $veja ['picture']?>' width = "400"></td>
+</tr>
+<tr>
+    <td> Prix: <?php echo $nike ['price']?> € </td>
+    <td> Prix: <?php echo $puma ['price']?> € </td>
+    <td> Prix: <?php echo $veja ['price']?> € </td>
+</tr>
+        <tr>
+            <td> Reduction: <?php echo $nike ['discount'] ?> %</td> <!--como botar o % fora do php e adiciona-lo sem precisar fazer uma concatenaçao-->
+            <td> Reduction: <?php echo $puma ['discount'] ?> %</td>
+            <td> Reduction: <?php echo $veja ['discount'] ?> % </td>
+        </tr>
+    </table>
 
 
     </body>
-</htm>
+</html>
