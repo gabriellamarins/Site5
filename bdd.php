dee831f274a3a275d5a5fe5f8771a07bb1cf4425
gabriellamarins <81614041+gabriellamarins@users.noreply.github.com>
2021-06-08 17:03:22 +0200

    Itération 5 - queries 1 table - queries multi tables - 1 query insert

---
+++ bdd.php
@@ -0,0 +1,101 @@
+<?php
+
+
+function queries() {
+
+      return $bdd = new PDO('mysql:host=localhost;dbname=gabriella_bdd;charset=utf8', 'gabriella38', 'qualauercoisa');
+
+}
+
+
+function first_query() {
+$bdd = queries();
+
+    $q1 = $bdd->query('SELECT * FROM products');
+
+while ($donnees = $q1->fetch()){
+
+      echo $donnees['name']. '<br />';
+    }
+
+    $q1->closeCursor();
+}
+
+
+?>
+
+
+
+
+
+
+<?php
+//Query 4 -
+
+function second_query() {
+    $bdd = queries();
+
+    $q2 = $bdd->query('SELECT orders.number, orders.date FROM orders WHERE orders.date > subdate(current_date, 10)');
+
+    while ($donnees2 = $q2->fetch()) {
+        echo $donnees2['number'] . "  -   " . $donnees2['date'] . '<br />';
+    }
+
+    $q2->closeCursor();
+}
+
+?>
+
+
+
+
+
+
+<?php
+//Query 3 - (query 5)
+function tird_query() {
+    $bdd = queries();
+
+    $q3 = $bdd->query('SELECT products.name, order_product.quantity, products.price FROM products JOIN order_product ON order_product.product_id = products.id WHERE order_product.order_id = 1');
+
+    while ($donnees3 = $q3->fetch()) {
+        echo $donnees3['name'] . "  -   " . $donnees3['quantity'] . $donnees3['price'] . '<br />';
+    }
+
+    $q3->closeCursor();
+}
+?>
+
+
+<?php
+//Query 4 - (query 6)
+
+function forth_query() {
+    $bdd = queries();
+    $q4 = $bdd->query('SELECT orders.number, SUM(order_product.quantity*products.price) AS total FROM orders LEFT JOIN order_product ON orders.id = order_product.order_id LEFT JOIN products ON order_product.product_id = products.id GROUP BY orders.number');
+
+    while ($donnees4 = $q4->fetch()) {
+        echo $donnees4['number'] . '<br />';
+
+    }
+
+    $q4->closeCursor();
+}
+?>
+
+<?php
+function insert_query() {
+$bdd = queries();
+
+$q5 = $bdd->query('INSERT INTO `orders` (`id`, `date`, `customer_id`,`number` ) VALUES (7, \'2021-06-03 16:46:32\', 1, \'\order00007\')');
+
+
+echo "nouveau produit ajouté" ;
+}
+
+
+?>
+
+
+
+
---
+++ database.php
@@ -0,0 +1,30 @@
+<?php
+include "bdd.php";
+?>
+
+<table style="width:100%">
+    <tr>
+        <td>
+            <h1> Liste de Produits </h1>
+            <?php echo first_query() ?>
+        </td>
+        <td>
+            <h1> Liste de Commandes </h1>
+            <?php echo second_query() ?>
+        </td>
+        <td>
+            <h1> Liste de Produits de la Commande 1 </h1>
+            <?php echo tird_query() ?>
+        </td>
+        <td>
+            <h1> Liste de Toutes les Commandes </h1>
+            <?php echo forth_query() ?>
+        </td>
+
+        <td>
+            <h1> INSERT new product </h1>
+            <?php echo insert_query() ?>
+        </td>
+    </tr>
+</table>
+
